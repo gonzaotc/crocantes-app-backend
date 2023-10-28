b7ce3230b58b22f0f9214522c7514d8788ecbef9
Gonzalo Othacehe <gonza.otc@gmail.com>
2023-10-27 22:15:43 -0300

    added run on server start for the updatecurrencyprices cron

--- src/crons/updateCurrencyPrices.cron.ts
+++ src/crons/updateCurrencyPrices.cron.ts
@@ -1,9 +1,14 @@
 import cron from "node-cron";
 import { updateCurrencyPrices } from "../modules/currencyType/service";
 
-// Running each 5 minutes.
-console.log("[CRON] Running UpdateCurrencyPrices each 5 minutes...");
-cron.schedule("*/5 * * * *", async () => {
+const updateCurrecyPricesCron = async () => {
   console.log("Updating Currency Types Prices...", new Date());
   await updateCurrencyPrices();
-});
+};
+
+// Run at server start
+updateCurrecyPricesCron();
+
+// Running each 5 minutes.
+console.log("[CRON] Running UpdateCurrencyPrices each 5 minutes...");
+cron.schedule("*/5 * * * *", updateCurrecyPricesCron);
