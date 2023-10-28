b74403874bdfc4d950bbfc9ccd14f409749131a8
Gonzalo Othacehe <gonza.otc@gmail.com>
2023-10-27 21:51:47 -0300

    removed process log

--- src/index.ts
+++ src/index.ts
@@ -5,6 +5,5 @@ import app from "./server";
 import "./crons/updateCurrencyPrices.cron";
 
 app.listen(config.port, "0.0.0.0", () => {
-  console.log(`running on http://localhost:${config.port}`);
-  console.log("process env", process.env);
+  console.log(`running on "0.0.0.0":${config.port}`);
 });
