c51463e0188e0644f9a7712d562dfc84e759af3e
Gonzalo Othacehe <gonza.otc@gmail.com>
2023-10-27 21:30:44 -0300

    zero port binding

--- src/index.ts
+++ src/index.ts
@@ -4,6 +4,7 @@ import config from "./config";
 import app from "./server";
 import "./crons/updateCurrencyPrices.cron";
 
-app.listen(config.port, () => {
+app.listen(config.port, "0.0.0.0", () => {
   console.log(`running on http://localhost:${config.port}`);
+  console.log("process env", process.env);
 });
